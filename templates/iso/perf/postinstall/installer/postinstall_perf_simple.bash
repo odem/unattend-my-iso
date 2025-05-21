d8752747aa2a2779f3714bc23d72fb4b9e976514
jb <jb@dev-bouche>
2025-05-21 11:10:45 +0200

    Some perf test changes

--- templates/iso/perf/postinstall/installer/postinstall_perf_simple.bash
+++ templates/iso/perf/postinstall/installer/postinstall_perf_simple.bash
@@ -8,7 +8,7 @@ SLEEP_BEFORE=1
 SLEEP_BETWEEN=1
 SLEEP_AFTER=1
 RESULT_DIR=/var/log/perf_results
-MAX_LOOPS=1000
+MAX_LOOPS=1
 FIO_SIZE=256
 FIO_LOOPS=5
 echo "-------------------------------------------------------------------------"
@@ -50,25 +50,33 @@ echo "#########################################################################"
 # Clean
 rm -rf $RESULT_DIR #2>/dev/null
 mkdir -p $RESULT_DIR #2>/dev/null
-pkill \$(which fio)
-echo "Cleaned"
+#pkill "\$(which fio)"
 
 # Iterate loops
-sleep $SLEEP_BEFORE
-for counter in \$(seq 1 $MAX_LOOPS); do
+sleep "$SLEEP_BEFORE"
+for counter in \$(seq 1 "$MAX_LOOPS"); do
     echo "---------------------------------------------------------------------"
     echo "- Perftest \$counter"
     echo "---------------------------------------------------------------------"
-    fio --loops=$FIO_LOOPS --size=${FIO_SIZE}m --filename="$RESULT_DIR/.fiomark.tmp" \
+    fio --loops="$FIO_LOOPS" --size="${FIO_SIZE}m" --output="$RESULT_DIR/.fiomark.txt" --filename="$RESULT_DIR/.fiomark.tmp" \
       --stonewall --ioengine=libaio --direct=1 --zero_buffers=0 --output-format=json \
-      --name=Seqread --bs=${FIO_SIZE}m --iodepth=1 --numjobs=1 --rw=read \
-      --name=Seqwrite --bs=${FIO_SIZE}m --iodepth=1 --numjobs=1 --rw=write \
-      2>&1 > $RESULT_DIR/perftest.log
-    sleep $SLEEP_BETWEEN
-    cat $RESULT_DIR/perftest.log
-    mv $RESULT_DIR/perftest.log $RESULT_DIR/result_\$counter.json
+      --name=Seqread --bs="${FIO_SIZE}m" --iodepth=1 --numjobs=1 --rw=read \
+      --name=Seqwrite --bs="${FIO_SIZE}m" --iodepth=1 --numjobs=1 --rw=write 2> $RESULT_DIR/errors.log
+    sleep "$SLEEP_BETWEEN"
+    cat "$RESULT_DIR/.fiomark.txt"
+    QUERY='def read_bw(name): [.jobs[] | select(.jobname==name+"read").read.bw] | add / 1024 | floor;
+       def read_iops(name): [.jobs[] | select(.jobname==name+"read").read.iops] | add | floor;
+       def write_bw(name): [.jobs[] | select(.jobname==name+"write").write.bw] | add / 1024 | floor;
+       def write_iops(name): [.jobs[] | select(.jobname==name+"write").write.iops] | add | floor;
+       def job_summary(name): read_bw(name), read_iops(name), write_bw(name), write_iops(name);
+       job_summary("Seq")'
+    read -d '\n' -ra V <<< "\$(jq "$QUERY" "$RESULT_DIR/.fiomark.txt")"
+    echo ""
+    echo "VEC: ${V[*]}"
+    echo "Read: ${V[0]}MB/s, Write: ${V[2]}MB/s"
+    #mv $RESULT_DIR/.fiomark.txt $RESULT_DIR/result_\$counter.json
 done
-sleep $SLEEP_AFTER
+sleep "$SLEEP_AFTER"
 echo "Done"
 exit 0
 EOF
