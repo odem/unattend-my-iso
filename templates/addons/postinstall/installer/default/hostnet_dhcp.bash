ff83d7bb78b47c1c91c45758d68e0d6fb18b64b4
jb <jb@fb2.fra-uas.de>
2025-06-17 15:46:31 +0200

    Improve debian12 template

--- templates/addons/postinstall/installer/default/hostnet_dhcp.bash
+++ templates/addons/postinstall/installer/default/hostnet_dhcp.bash
@@ -13,6 +13,9 @@ echo "-------------------------------------------------------------------------"
 echo ""
 sleep 1
 
+#default dns
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+
 # Create network config
 cat <<EOF > /etc/network/interfaces
 
