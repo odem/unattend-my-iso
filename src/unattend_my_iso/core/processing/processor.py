2190aa2573e7ab9f210e06e47d3a2529ebc716d3
jb <jb@fb2.fra-uas.de>
2026-04-30 09:55:15 +0200

    Add some more logging in main processor

--- src/unattend_my_iso/core/processing/processor.py
+++ src/unattend_my_iso/core/processing/processor.py
@@ -1,4 +1,5 @@
 import sys
+from datetime import datetime
 from unattend_my_iso.common.config import TaskResult, TemplateConfig
 from unattend_my_iso.common.logging import log_debug, log_error, log_info, log_warn
 from unattend_my_iso.core.reader.reader_config import TaskConfig, get_configs
@@ -12,24 +13,29 @@ from unattend_my_iso.core.processing.processor_task_networking import (
 class UmiTaskProcessor(
     TaskProcessorIsogen, TaskProcessorVmRun, TaskProcessorNetworking
 ):
+    proc_time_start: datetime
+    proc_time_stop: datetime
+    topic: str
 
     def __init__(self):
+        self.topic = self.__class__.__qualname__
         TaskProcessorIsogen.__init__(self)
         TaskProcessorVmRun.__init__(self)
 
     def do_process(self):
         taskconfigs = get_configs()
-        log_info(f"TaskConfigs : {len(taskconfigs)}", self.__class__.__qualname__)
+        log_info(f"TaskConfigs : {len(taskconfigs)}",
+                 self.topic)
         if len(taskconfigs) > 0:
             for cfg in taskconfigs:
                 if isinstance(cfg, TaskConfig):
                     log_info(
                         f"TaskTemplate: {cfg.target.template}",
-                        self.__class__.__qualname__,
+                        self.topic,
                     )
                     log_info(
                         f"TaskOverlay : {cfg.target.template_overlay}",
-                        self.__class__.__qualname__,
+                        self.topic,
                     )
                     self._get_addons()
                     self._get_templates(cfg)
@@ -37,34 +43,32 @@ class UmiTaskProcessor(
                     template = self._get_task_template(cfg)
                     if template is None:
                         log_error(
-                            f"TemplateErr : Template is None. Current={template}",
-                            self.__class__.__qualname__,
+                            f"TemplateErr : Template is None. Current={template}", self.topic,
                         )
                         sys.exit(2)
                     if template is not None:
+                        self.proc_time_start = datetime.now()
                         result = self._process_task(cfg, template)
                         self._process_result(result)
+                        self.proc_time_stop = datetime.now()
+                        self._log_result(result)
                 else:
-                    log_error(
-                        f"TaskConfig invalid: {taskconfigs}",
-                        self.__class__.__qualname__,
-                    )
+                    log_error(f"TaskConfig invalid: {taskconfigs}", self.topic)
         else:
             log_error(
-                "No Taskconfig available after parsing arguments",
-                self.__class__.__qualname__,
+                "No Taskconfig available after parsing arguments", self.topic,
             )
             sys.exit(1)
 
     def _process_task(self, args: TaskConfig, template: TemplateConfig) -> TaskResult:
         log_info(
             f"Template: {args.target.template} ({args.target.template_overlay})",
-            self.__class__.__qualname__,
+            self.topic,
         )
         tasktype = args.target.proctype
         if args.run.verbosity >= 4:
-            log_debug(f"TaskConfig : {template}", self.__class__.__qualname__)
-            log_debug(f"TemplateConfig : {args}", self.__class__.__qualname__)
+            log_debug(f"TaskConfig : {template}", self.topic)
+            log_debug(f"TemplateConfig : {args}", self.topic)
         if tasktype == "build_all":
             return self.task_build_all(args)
         if tasktype == "extract":
@@ -91,6 +95,26 @@ class UmiTaskProcessor(
 
     def _process_result(self, result: TaskResult):
         if result.success:
-            log_debug(f"Task Success : {result.success}", self.__class__.__qualname__)
+            log_debug(f"Task Success : {result.success}", self.topic)
         else:
-            log_error(f"Task Error : Msg:{result.msg}", self.__class__.__qualname__)
+            log_error(f"Task Error : Msg:{result.msg}", self.topic)
+
+    def _log_result(self, result: TaskResult):
+        dformat = "%Y-%m-%d %H:%M:%S"
+        diff = self.proc_time_stop - self.proc_time_start
+        start = datetime.strftime(self.proc_time_start, dformat)
+        stop = datetime.strftime(self.proc_time_stop, dformat)
+        total_seconds = int(diff.total_seconds())
+        diff_ms = diff.microseconds / 1000
+        diff_text = f"{diff_ms} ms"
+        if total_seconds > 0:
+            diff_text = f"{total_seconds} s"
+        msg = f"{result.success}"
+        if result.success is False:
+            msg = f"{result.success} -> {result.msg_short}"
+        log_info("--- Result -----------------------------------", self.topic)
+        log_info(f"Result     : {msg}", self.topic)
+        log_info(f"Time Start : {start}", self.topic)
+        log_info(f"Time Stop  : {stop}", self.topic)
+        log_info(f"Total Time : {diff_text}", self.topic)
+        log_info("----------------------------------------------", self.topic)
